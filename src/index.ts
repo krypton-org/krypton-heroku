1d9c27bed7c5dd2e340d83fccc38c6db10669938
jrebecchi <johann.corcuff@gmail.com>
2020-02-24 01:51:52 +0100

    Setting up heroku app.json

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,6 @@ import express from 'express';
 import { GraphQLAuthService } from 'graphql-auth-service';
 
 const app = express();
-console.log(process.env.MONGODB_URI);
-console.log("yooooooooooooooooooo");
 
-const config = {
-    dbConfig: {
-        address: process.env.MONGODB_URI
-    }
-}
-
-app.use(GraphQLAuthService( config ));
+app.use(GraphQLAuthService({ dbAddress: process.env.MONGODB_URI as string }));
 app.listen(process.env.PORT || 5000, () => { console.log("Listening on port " + process.env.PORT || 5000) });
