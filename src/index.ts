97306d00565c79b77276e4bda1059f34d99b9fd1
Johann Rebecchi <johann.corcuff@gmail.com>
2020-03-26 00:16:17 +0100

    Updating deployment with Krypton Authentication v1

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 import express from 'express';
-import { GraphQLAuthService } from 'graphql-auth-service';
-
+import KryptonAuth from '@krypton-org/krypton-auth';
+import cors from 'cors';
 const app = express();
+app.use(cors({
+    origin: function(origin, callback){
+        return callback(null, true);
+    },
+    credentials: true,
+}));
 
-app.use(GraphQLAuthService({ dbAddress: process.env.MONGODB_URI as string }));
+app.use(KryptonAuth({ dbAddress: process.env.MONGODB_URI as string }));
 app.listen(process.env.PORT || 5000, () => { console.log("Listening on port " + process.env.PORT || 5000) });
